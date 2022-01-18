7793aa7e704c219898bf42b347a99725074aa020
Gabriel Salinas <g.szada@gmail.com>
2022-01-18 12:54:01 +0100

    Tall piece working properly on new engine

--- newMain.js
+++ newMain.js
@@ -4,17 +4,16 @@ const canvas = document.getElementById("canvas"),
       ctx = canvas.getContext("2d");
 
 const greenBlock = new Image(),
-      tallGrey = new Image(),
-      greyBrick = new Image();
+      tallGreen = new Image(),
+      greyBrick = new Image(),
+      longGreenBrick = new Image();
       
 greenBlock.src = "images/greenTile.png";
 greyBrick.src = "images/horizontalGrey.png";    
-tallGrey.src = "images/tallGrey.png"; 
-
-// The matrix will save the color and a boolean for place occupancy
+tallGreen.src = "images/tallGreen.png"; 
+longGreenBrick.src = "images/longGreenBrick.png"
 
 // matrix[rowIndex][columnIndex]
-
 var matrix = [
   [{},{},{},{},{},{}],  
   [{},{},{},{},{},{}],  
@@ -28,16 +27,11 @@ var matrix = [
   [{},{},{},{},{},{}]
 ];
 
-const column_ini = 4;
-
-var maxRow_index = matrix.length - 1;
-
-var maxColumn_index = matrix[0].length - 1;
+var maxRow_index = matrix.length - 1,
+    maxColumn_index = matrix[0].length - 1;
 
 // Pieces arrays
 var pieces = [];
-
-
 
 // Movement variables
 const speed = 1, 
@@ -45,31 +39,27 @@ const speed = 1,
 
 var right = false,
     left = false,
-    down = false;
-
-var timeOut = true;
+    down = false,
+    up = false;
 
 // Other global variables   
 var score = 0,
-    timeOut = false,
     frames = 0, 
-    isGameOver = false;
+    isGameOver = false,
+    timeOut = false,
+    tallInPlay = false;
 
 /**
- * @abstract
- * 
- * Pieces
+ * @abstract Piece classes
  * 
  */
-
- class block {
+class block {
   constructor(){
     this.type = "block"
     this.color = "green",
     this.image = greenBlock,
     this.x = 120,
     this.y = 0,
-    this.isMoving = true,
     this.isActive = true,
     this.usingColumns = [3],
     this.usingRows = [0]
@@ -83,7 +73,6 @@ class brick {
     this.image = greyBrick,
     this.x = 120,
     this.y = 0,
-    this.isMoving = true,
     this.isActive = true,
     this.usingColumns = [3, 4],
     this.usingRows = [0]
@@ -93,15 +82,92 @@ class brick {
 class tall{
   constructor(){
     this.type = "tall",
-    this.color = "grey",
-    this.image = tallGrey,
+    this.isVertical = true,
+    this.color = "green",
+    this.image = tallGreen,
     this.x = 120,
     this.y = 0,
-    this.isMoving = true,
     this.isActive = true,
     this.usingColumns = [3],
-    this.usingRows = [0, 1]
+    this.usingRows = [0, 1, 2]
   }
+}
+
+function rotateTall(p){
+
+  if(p.isVertical){
+
+    /**
+     * Before rotating let's check if the piece can rotate
+     */
+
+    // Check canvas borders
+    if(p.usingColumns[0] > 0 && p.usingColumns < maxColumn_index){
+
+      let M = matrix,
+          pieceColumn = p.usingColumns[0],
+          pieceRow = p.usingRows,
+          pieceMiddleRow = p.usingRows[1],
+          left_fragment_1 = M[pieceRow[0]][pieceColumn - 1],
+          left_fragment_2 = M[pieceRow[1]][pieceColumn - 1],
+          left_fragment_3 = M[pieceRow[2]][pieceColumn - 1],
+          right_fragment_1 = M[pieceRow[0]][pieceColumn + 1],
+          right_fragment_2 = M[pieceRow[1]][pieceColumn + 1],
+          right_fragment_3 = M[pieceRow[2]][pieceColumn + 1];
+
+      // Rotating area conditions
+      let c = [
+        !left_fragment_1.isOccupied,
+        !left_fragment_2.isOccupied,
+        !left_fragment_3.isOccupied,
+        !right_fragment_1.isOccupied,
+        !right_fragment_2.isOccupied,
+        !right_fragment_3.isOccupied
+      ]
+
+      // Is rotation possible ?
+      if(c[0] && c[1] && c[2] && c[3] && c[4] && c[5]){
+        p.isVertical = false
+        p.x -= 40
+        p.y += 40
+        p.usingColumns = [ pieceColumn - 1,  pieceColumn, pieceColumn + 1 ]
+        p.usingRows = [ pieceMiddleRow ] 
+        p.image = longGreenBrick
+      }
+
+    }
+  }
+  
+  else if(!p.isVertical){
+
+    /**
+     * Before rotating let's check if the piece can rotate
+     */
+    let M = matrix,
+        pieceColumn = p.usingColumns,
+        pieceRow = p.usingRows[0],
+        topLeft_fragment = M[pieceRow - 1][pieceColumn[0]],
+        topMiddle_fragment = M[pieceRow - 1][pieceColumn[1]],
+        topRight_fragment = M[pieceRow - 1][pieceColumn[2]];
+
+    // Rotation area conditions    
+    let c = [
+      !topLeft_fragment.isOccupied,
+      !topMiddle_fragment.isOccupied,
+      !topRight_fragment.isOccupied
+    ]
+
+    if(c[0] && c[1] && c[2]){
+
+      p.isVertical = true
+      p.x += 40
+      p.y -= 40
+      p.usingColumns = [ pieceColumn[1] ]
+      p.usingRows = [ pieceRow - 1, pieceRow, pieceRow + 1 ] 
+      p.image = tallGreen
+    }
+  }
+
 }
 
 var frames = 0
@@ -131,6 +197,16 @@ function gameLoop(){
     p.isActive ? AP = p : null
   })
 
+  /**
+   * @abstract Rotation
+   * 
+   */
+  if(up && AP.type === "tall" && !timeOut){
+    
+    rotateTall(AP)
+    timeOut = true;
+    setTimeout(() => { timeOut = false }, 120);
+  }
 
 
   /** 
@@ -167,20 +243,45 @@ function gameLoop(){
     }
 
     if(AP.type === "tall"){
-      if(AP['usingRows'][1] + 1 < lowestAvailableRow){
-        
-        // Update rows of piece
-        AP['usingRows'][0] += 1
-        AP['usingRows'][1] += 1
 
-        // Update coordinate y
-        AP.y += 40 
+      if(AP.isVertical){
+
+        if(AP['usingRows'][2] + 1 < lowestAvailableRow){
+        
+          // Update rows of piece
+          AP['usingRows'][0] += 1
+          AP['usingRows'][1] += 1
+          AP['usingRows'][2] += 1
+  
+          // Update coordinate y
+          AP.y += 40 
+        }
+        else{ 
+          // Deactivate piece and create a new one
+          AP.isActive = false
+          pieces.push(randomPiece())
+        }
+
       }
-      else{ 
-        // Deactivate piece and create a new one
-        AP.isActive = false
-        pieces.push(randomPiece())
+
+      if(AP.isVertical === false){
+
+        if(AP['usingRows'][0] + 1 < lowestAvailableRow){
+        
+          // Update rows of piece
+          AP['usingRows'][0] += 1
+  
+          // Update coordinate y
+          AP.y += 40 
+        }
+        else{ 
+          // Deactivate piece and create a new one
+          AP.isActive = false
+          pieces.push(randomPiece())
+        }
+
       }
+      
     }          
 
     // Reset frame count
@@ -194,54 +295,130 @@ function gameLoop(){
    * HORIZONTAL MOVEMENT
    * 
    */
-  let left_fragment = matrix[ AP['usingRows'][0] ][ AP['usingColumns'][0] - 1 ]
-  
-    if(left && AP['usingColumns'][0] > 0 && !timeOut && !left_fragment.isOccupied){
-      
-      if(AP.type === "block" || AP.type === "tall"){
-        AP.usingColumns[0] -= 1
+  if(left && !timeOut){
+    
+    let left_fragment
+
+    if(AP.type === "tall"){
+
+      if(AP.isVertical){
+        left_fragment = matrix[ AP.usingRows[2] ][ AP.usingColumns[0] - 1 ]
+
+        // Can it move to the left?
+        if(AP.usingColumns[0] > 0 && !left_fragment.isOccupied){
+        
+          AP.usingColumns[0] -= 1
+          AP.x -= 40;
+        }
       }
-      
-      if(AP.type === "brick"){
-        AP.usingColumns[0] -= 1
-        AP.usingColumns[1] -= 1
+
+      else if(!AP.isVertical){
+        left_fragment = matrix[ AP.usingRows[0] ][ AP.usingColumns[0] - 1 ]
+
+         // Can it move to the left?
+        if(AP['usingColumns'][0] > 0 && !left_fragment.isOccupied){
+
+          AP.usingColumns[0] -= 1
+          AP.usingColumns[1] -= 1
+          AP.usingColumns[2] -= 1
+          AP.x -= 40;
+        }
       }
-      
-      AP.x -= 40;
-      timeOut = true;
-      setTimeout(() => { timeOut = false }, 120);
+
     }
 
-  if(right){
+    if(AP.type !== "tall"){
+
+      left_fragment = matrix[ AP.usingRows[0] ][ AP.usingColumns[0] - 1 ]
+
+      if(AP.usingColumns[0] > 0 && !left_fragment.isOccupied){
+
+        switch(AP.type){
+
+          case "block":
+            AP.usingColumns[0] -= 1
+            AP.x -= 40;
+            break
+
+          case "brick":
+            AP.usingColumns[0] -= 1
+            AP.usingColumns[1] -= 1
+            AP.x -= 40;
+        }
+
+      }     
+    }
+
+    timeOut = true;
+    setTimeout(() => { timeOut = false }, 120);
+  }
+  
+
+  if(right && !timeOut){
   
     let right_fragment
 
-      if(AP.type === 'block' || AP.type === 'tall'){ 
-      
-          right_fragment = matrix[ AP['usingRows'][0] ][ AP['usingColumns'][0] + 1 ]
-          
-          if(AP['usingColumns'][0] < maxColumn_index && !timeOut && !right_fragment.isOccupied){
-            AP['usingColumns'][0] += 1
-            AP.x += 40
-            timeOut = true
-            setTimeout(() => { timeOut = false }, 120)
-          }
+    if(AP.type === "tall"){
+
+      if(AP.isVertical){
+        right_fragment = matrix[ AP.usingRows[2] ][ AP.usingColumns[0] + 1 ]
+
+        // Can it move to the right?
+        if(AP.usingColumns[0] < maxColumn_index && !right_fragment.isOccupied){
+        
+          AP.usingColumns[0] += 1
+          AP.x += 40;
+        }
       }
 
+      else if(!AP.isVertical){
+        right_fragment = matrix[ AP.usingRows[0] ][ AP.usingColumns[0] - 1 ]
 
-      if(AP.type === 'brick'){
-      
-          right_fragment = matrix[AP.usingRows[0]][AP.usingColumns[1] + 1]
+         // Can it move to the right?
+        if(AP['usingColumns'][0] > 0 && !right_fragment.isOccupied){
 
-          if(AP['usingColumns'][1] < maxColumn_index && !timeOut && !right_fragment.isOccupied){
-            AP['usingColumns'][0] += 1
-            AP['usingColumns'][1] += 1
-            AP.x += 40
-            timeOut = true
-            setTimeout(() => { timeOut = false }, 120)
-          }
+          AP.usingColumns[0] += 1
+          AP.usingColumns[1] += 1
+          AP.usingColumns[2] += 1
+          AP.x += 40;
+        }
       }
-    
+
+    }
+
+    if(AP.type !== "tall"){
+
+        switch(AP.type){
+
+          case "block":
+
+            right_fragment = matrix[ AP.usingRows[0] ][ AP.usingColumns[0] + 1 ]
+
+            if(AP.usingColumns[0] < maxColumn_index && !right_fragment.isOccupied){
+              
+              AP.usingColumns[0] += 1
+              AP.x += 40
+            }
+            break
+
+          case "brick":
+
+            right_fragment = matrix[ AP.usingRows[0] ][ AP.usingColumns[1] + 1 ]
+
+            if(AP.usingColumns[1] < maxColumn_index && !right_fragment.isOccupied){
+
+              AP.usingColumns[0] += 1
+              AP.usingColumns[1] += 1
+              AP.x += 40
+            }
+            break
+        }
+
+           
+    }
+
+    timeOut = true;
+    setTimeout(() => { timeOut = false }, 120);
   }
   
 
@@ -252,21 +429,21 @@ function gameLoop(){
   
   let savedRows = []
 
-  matrix.forEach((rowArray, rowIndex) => {
+  matrix.forEach((rowFragments, rowIndex) => {
 
     let brick_fragmentCount = 0,
         greenCount = 0,
-        tall_inRow = false;
+        vertical_tall_inRow = false;
 
-    rowArray.forEach((fragment) => { // Loop through row columns
+    rowFragments.forEach((fragment) => { // Loop through row columns
 
       if(fragment.isOccupied && fragment.pieceIsParked){
         
-        fragment.type === "tall" ? tall_inRow = true : null
+        fragment.piecePosition === "vertical" ? vertical_tall_inRow = true : null
 
         fragment.type === "brick" ? brick_fragmentCount++ : null
 
-        fragment.color === "green" ? greenCount++ : null
+        fragment.color === "green" && fragment.piecePosition !== "vertical"? greenCount++ : null
 
       }
     })
@@ -277,6 +454,9 @@ function gameLoop(){
     ]
 
     if(conditions[0]){
+
+      console.log('condition 0 met')
+
       pieces = pieces.filter(p => {
                   if(p['usingRows'][0] === rowIndex){
                     return false // Remove
@@ -288,18 +468,18 @@ function gameLoop(){
     }
 
     // Register the row if there is a tall piece
-    conditions[1] && tall_inRow ? savedRows.push(rowIndex) : null
+    conditions[1] && vertical_tall_inRow ? savedRows.push(rowIndex) : null
     
     /**
      * If there are two rows matching colors with a tall piece in it, go 
      * ahead and remove the pieces.
      */
-    if(savedRows.length === 2){
+    if(savedRows.length === 3){
 
       for(const row of savedRows)
 
       pieces = pieces.filter(p => {
-        if(p['usingRows'][0] === row){
+        if(p.usingRows[0] === row){
           return false // Remove
         }
         else{
@@ -340,7 +520,8 @@ function gameLoop(){
         color: null, 
         type: null,
         isOccupied: false,
-        pieceIsParked: false
+        pieceIsParked: false,
+        piecePosition: null
       }
 
     })
@@ -359,6 +540,10 @@ function gameLoop(){
         fragment.isOccupied = true
 
         p.isActive ? null : fragment.pieceIsParked = true
+
+        if(p.type === "tall"){
+          p.isVertical ? fragment.piecePosition = "vertical" : null
+        }
 
       })
     })
@@ -405,16 +590,37 @@ function gameLoop(){
 
         case "tall":
 
-          if(!p.isActive && p['usingRows'][1] < lowestAvailableRow){
-        
-            let pieceInRow = p['usingRows'][1]
-    
-            let delta = lowestAvailableRow - pieceInRow
-            
-            p.y += 40 * delta;
-            p['usingRows'][0] = lowestAvailableRow - 1;
-            p['usingRows'][1] = lowestAvailableRow;
+          if(p.isVertical){
+
+            if(!p.isActive && p['usingRows'][2] < lowestAvailableRow){
+          
+              let pieceInRow = p['usingRows'][2]
+      
+              let delta = lowestAvailableRow - pieceInRow
+              
+              p.y += 40 * delta;
+              p['usingRows'][0] = lowestAvailableRow - 2;
+              p['usingRows'][1] = lowestAvailableRow - 1;
+              p['usingRows'][2] = lowestAvailableRow;
+
+            }
           }
+
+          if(p.isVertical === false){
+
+            if(!p.isActive && p['usingRows'][0] < lowestAvailableRow){
+          
+              let pieceInRow = p['usingRows'][0]
+    
+              let delta = lowestAvailableRow - pieceInRow
+
+              p.y += 40 * delta;
+              p['usingRows'][0] = lowestAvailableRow;
+
+            }
+
+          }
+
           break
 
       }
@@ -462,7 +668,15 @@ function GET_lowestAvailableRow(piece){
         break
 
       case "tall":
-        initialRow = piece['usingRows'][1] + 1 
+
+        if(piece.isVertical){
+          initialRow = piece['usingRows'][2] + 1 
+        }
+
+        if(piece.isVertical === false){
+          initialRow = piece['usingRows'][0] + 1 
+        }
+
         break
 
     }
@@ -493,24 +707,26 @@ function randomPiece(){
   let rand = Math.random()
 
   /**
-   * Only one tall piece is allowed to be in play
-   * 
-   * Note: this condition makes the matching colors work properly
-   * with the tall piece
-   */ 
-  let tall_inPlay = false
+   * Only one tallInPlay is allowed to be in play,
+   * having more would create many issues..
+   */
 
-  for(const piece of pieces){
-    if(piece.type === "tall"){
-      tall_inPlay = true
-      break
+  // Is there a tallInPlay?
+  for(const p of pieces){
+    
+    if(p.type === "tall"){
+     tallInPlay = true
+     break
+    }
+    else {
+     tallInPlay = false
     }
   }
 
-  if(rand < 0.3 && !tall_inPlay){
+  if(rand < 0.3 && !tallInPlay){
     return new tall()
   }
-  else if(rand < 0.7){
+  else if(rand < 0.5){
     return new block()
   }
   else{
@@ -537,6 +753,9 @@ function handleKeyDown(e){
   if(e.key === "Down" || e.key === "ArrowDown"){
     down = true;
   }
+  if(e.key === "Up" || e.key === "ArrowUp"){
+    up = true;
+  }
 }
 function handleKeyUp(e){
   if(e.key === "Right" || e.key === "ArrowRight"){
@@ -547,6 +766,9 @@ function handleKeyUp(e){
   }
   if(e.key === "Down" || e.key === "ArrowDown"){
     down = false;
+  }
+  if(e.key === "Up" || e.key === "ArrowUp"){
+    up = false;
   }
 }
 
